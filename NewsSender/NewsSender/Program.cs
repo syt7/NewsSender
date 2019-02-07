6de8b6071adb3b44054797f336c690c162c9ebae
syt7 <martin@syt7.co.uk>
2019-02-07 22:04:25 +0000

    First Commit - .netCore Console App

---
+++ NewsSender/NewsSender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewsSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
